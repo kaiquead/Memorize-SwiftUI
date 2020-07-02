3efc5221363f728b77a0e7104d612da23aad8b35
kaiquead <36281679+kaiquead@users.noreply.github.com>
2020-07-02 00:34:58 -0300

    Modified the Model View to make random numbers of pairs

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -11,10 +11,12 @@ import SwiftUI
 class EmojiMemoryGame{
     
     private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
         
     static func createMemoryGame() -> MemoryGame<String>{
-        let emojis: Array<String> = ["👻", "🎃", "🕷"]
-        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+        let numberAleatoryOfPairs = Int.random(in: 2...5)
+        let emojis: Array<String> = ["👻", "🎃", "🕷", "🧙🏼‍♀️", "🧹", "🍬"]
+        return MemoryGame<String>(numberOfPairsOfCards: numberAleatoryOfPairs) { pairIndex in
             return emojis[pairIndex]
         }
     }
